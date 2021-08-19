cc90b7fe7e70e1ebf2596e1e82e60d4ebae943ae
JiAn <JiAncs010723@outlook.com>
2021-08-19 13:25:28 +0800

    路径二次修改

--- src/main/java/search/Const.java
+++ src/main/java/search/Const.java
@@ -1,9 +1,9 @@
 package search;
 
 public class Const {
-    public static final String SAVE_PATH = "~/trpgsearch/src/main/resources/lucene_db";
-    public static final String HTML_PATH = "~/lucene_db/save";
-    public static final String INDEX_PATH = "~/lucene_db";
+    public static final String SAVE_PATH = "/root/trpgsearch/src/main/resources/lucene_db";
+    public static final String HTML_PATH = "/root/lucene_db/save";
+    public static final String INDEX_PATH = "/root/lucene_db";
 
     public static final String PATH_FIELD_NAME = "docPath";
     public static final String URL_FIELD_NAME = "docUrl";
