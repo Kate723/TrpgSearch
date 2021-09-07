19b6719d158c98302a7a6b317393f8c03b4aec75
JiAn <2726428885@qq.com>
2021-09-07 20:36:09 +0800

    config root

--- src/main/java/search/Const.java
+++ src/main/java/search/Const.java
@@ -1,9 +1,11 @@
 package search;
 
 public class Const {
-    public static final String SAVE_PATH = "/root/trpgsearch/src/main/resources/lucene_db";
-    public static final String HTML_PATH = "/root/lucene_db/save";
-    public static final String INDEX_PATH = "/root/lucene_db";
+    public static final String ROOT_PATH = "/usr/share/nginx/html/"
+
+    public static final String SAVE_PATH = ROOT_PATH + "trpgsearch/src/main/resources/lucene_db";
+    public static final String HTML_PATH = ROOT_PATH + "lucene_db/save";
+    public static final String INDEX_PATH = ROOT_PATH + "lucene_db";
 
     public static final String PATH_FIELD_NAME = "docPath";
     public static final String URL_FIELD_NAME = "docUrl";
